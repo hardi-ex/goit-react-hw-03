978f36f25017cde9315de1c4e46b5f5e37531657
Yaroslav <skylandtat@ukr.net>
2024-06-15 13:37:51 +0200

    Fix: contact and contactList

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,20 +2,29 @@ import css from "./Contact.module.css";
 import { FaPhone } from "react-icons/fa6";
 import { IoPersonSharp } from "react-icons/io5";
 
-export const Contact = ({ contact }) => {
+export const Contact = ({ contact, onDelete }) => {
   const { name, number } = contact;
 
   return (
     <>
-      <div className={css.div}>
-        <p className={css.text}>
-          <IoPersonSharp className={css.icon} />
-          {name}
-        </p>
-        <p className={css.text}>
-          <FaPhone className={css.icon} />
-          {number}
-        </p>
+      <div className={css.mainDiv}>
+        <div className={css.div}>
+          <p className={css.text}>
+            <IoPersonSharp className={css.icon} />
+            {name}
+          </p>
+          <p className={css.text}>
+            <FaPhone className={css.icon} />
+            {number}
+          </p>
+        </div>
+        <button
+          className={css.btn}
+          type="button"
+          onClick={() => onDelete(contact.id)}
+        >
+          Delete
+        </button>
       </div>
     </>
   );
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -7,14 +7,7 @@ export const ContactList = ({ contactsList, deleteContact }) => {
       {contactsList.map((contact) => {
         return (
           <li className={css.li} key={contact.id}>
-            <Contact contact={contact} deleteContact={deleteContact} />
-            <button
-              className={css.btn}
-              type="button"
-              onClick={() => deleteContact(contact.id)}
-            >
-              Delete
-            </button>
+            <Contact contact={contact} onDelete={deleteContact} />
           </li>
         );
       })}
